595646a3075f6002800d27a39a50d7757070456e
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2022-05-19 02:42:09 +0530

    initial commit

---
+++ contracts/Membership.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Whitelist {
+    function _burn(
+        address,
+        uint256,
+        uint256
+    ) public {}
+}
+
+contract Membership is ERC721URIStorage, Ownable {
+    struct set {
+        uint256 upper_index;
+        uint256 lower_index;
+    }
+    mapping(string => set) hair;
+    mapping(string => set) background;
+    mapping(string => set) gender;
+    mapping(uint256 => uint256) remainingNFTS;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Member", "MEM") {}
+
+    function extendAuction(uint256 blocktime)
+        public
+        onlyOwner
+        returns (uint256)
+    {}
+
+    function giveMembership(
+        string memory _hair,
+        string memory _background,
+        string memory _gender,
+        address _originalNFT
+    ) public onlyOwner returns (uint256) {
+        // Burn the original NFT
+        Whitelist(addr)
+        
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
---
+++ contracts/Whitelist.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract Whitelist {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+}
