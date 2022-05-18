e4ff9186160ca940a2105536f0103ea2320fc472
Vaibhav Maheshwari <vaibhav.dkm@gmail.com>
2022-05-19 03:00:52 +0530

    intersection

--- contracts/Membership.sol
+++ contracts/Membership.sol
@@ -25,11 +25,15 @@ contract Membership is ERC721URIStorage, Ownable {
     mapping(string => set) background;
     mapping(string => set) gender;
     mapping(uint256 => uint256) remainingNFTS;
-
+    address whitelist;
+    uint256 nftRemainingCount;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Member", "MEM") {}
+    constructor(address _whitelist, uint256 _totalNFT) ERC721("Member", "MEM") {
+        whitelist = _whitelist;
+        nftRemainingCount = _totalNFT;
+    }
 
     function extendAuction(uint256 blocktime)
         public
@@ -41,15 +45,25 @@ contract Membership is ERC721URIStorage, Ownable {
         string memory _hair,
         string memory _background,
         string memory _gender,
-        address _originalNFT
+        address _originalTokenId,
+        address _from
     ) public onlyOwner returns (uint256) {
         // Burn the original NFT
-        Whitelist(addr)
-        
+        Whitelist(whitelist)._burn(_from, _originalTokenId, 1);
+
+        // Find the right NFT
+
         _mint(player, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
-        _tokenIds.increment();
+        nftRemainingCount.decrement();
         return newItemId;
     }
+
+    modifier intersection(
+        uint256 _a,
+        uint256 _b,
+        uint256 _c,
+        uint256 _d
+    ) {}
 }
